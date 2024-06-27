8da0362b8450b0b90a9f438f5d0d5f4081095b3b
Dickson <128027259+Yu-Chenxu@users.noreply.github.com>
2024-06-27 21:38:13 +0800

    Update main.py
    add encoding='utf-8'

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 
 crawler = Crawler()
 
-list = crawler.query(search_term="Meta analysis JAMA eye disease", max_results=2)
+list = crawler.query(search_term="Meta analysis JAMA eye disease", max_results=8)
 
 # export data to json file
-with open('data.json', 'w') as f:
+with open('data.json', 'w', encoding='utf-8') as f:
   json.dump({ 'data': list }, f, ensure_ascii=False, indent = 4)
