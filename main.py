be5d63049baeebd42541c73e6ca5f9506c11cf76
Iafcy622 <frankiecheung512@gmail.com>
2024-07-01 12:03:58 +0800

    Minor changes

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-API_KEY = os.environ['PUBMED_API_KEY']
+PUBMED_API_KEY = os.environ['PUBMED_API_KEY']
 
 crawler = Crawler()
 
-list = crawler.query(search_term="Meta analysis JAMA eye disease", max_results=10, api_key=None)
+list = crawler.query(search_term="Meta analysis JAMA eye disease", max_results=10, api_key=PUBMED_API_KEY)
 crawler.download(list)
