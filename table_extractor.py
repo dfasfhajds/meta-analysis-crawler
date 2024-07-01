26d854b78b1f7408434e60eda4a26fce6de38703
Iafcy622 <frankiecheung512@gmail.com>
2024-07-01 17:16:59 +0800

    Changed file type from xlsx to csv
    Tables extracted will be saved as csv inside the directory of the article
    Excel file is kept for checking

--- table_extractor.py
+++ table_extractor.py
@@ -19,14 +19,16 @@ def get_title(words: List[Dict[str, str]]) -> Optional[str]:
         return title if title else None
     return None
 
-def extract_table(pdf_path: str, writer: pd.ExcelWriter, pmid: str):
+def extract_table(pdf_path: str, pmid: str, csv_path: str, writer: pd.ExcelWriter):
     """
-    Extract tables from a PDF file and save them into an Excel sheet.
+    Extract tables from a PDF file and save them into CSV files in the article directory
+    Excel file is also saved for checking
 
     Parameters:
         pdf_path (str): The path to the PDF file.
-        writer (pd.ExcelWriter): The Excel writer object to save sheets.
         pmid (str): Article PMID
+        csv_path (str): path for CSV
+        writer (pd.ExcelWriter): The Excel writer object to save sheets.
     """
     keywords = ["quality", "risk of bias", "included"]
     with pdfplumber.open(pdf_path) as pdf:
@@ -39,18 +41,18 @@ def extract_table(pdf_path: str, writer: pd.ExcelWriter, pmid: str):
                 title = get_title(words)
                 if title and any(keyword in title.lower() for keyword in keywords):
                     # print(f"Found title: {title}")
+                    table = [[cell.replace('\n',' ') if cell else cell for cell in row] for row in table] # remove '\n' in all cells
                     table_title = title
                     result = pd.DataFrame(table[1:], columns=table[0])
                     break  # Assume we only need the first matching table per page
 
-        sheet_name = f"{pmid}_{table_title.split('.')[0].replace(' ', '_')}" if table_title else f"{pmid}"
+        filename = f"{pmid}_{table_title.split('.')[0].replace(' ', '_')}" if table_title else f"{pmid}"
         
-        # Ensure at least one worksheet is created
-        if result.empty:
-            result = pd.DataFrame({'Message': ["No relevant tables found"]})
-
-        # print(f"Writing to sheet: {sheet_name} with {len(result)} records")
-        result.to_excel(writer, sheet_name=sheet_name, index=False)
+        # Only create csv in path if table is successfully extracted
+        if not result.empty:
+            result.to_csv(f"{csv_path}/{filename}.csv", index=False, encoding="utf-8")
+            result.to_excel(writer, sheet_name=filename, index=False)
+            # print(f"Writing to {csv_path}/{filename}.csv with {len(result)} records")
 
 def read_table(base_dir: str = "./data", output_file: str = "output.xlsx"):
     """
@@ -60,7 +62,7 @@ def read_table(base_dir: str = "./data", output_file: str = "output.xlsx"):
         base_dir (str): The base directory to search for PDF files.
         output_file (str): The path to save the output Excel file.
     """
-    with pd.ExcelWriter(f"./data/{output_file}") as writer:
+    with pd.ExcelWriter(f"{base_dir}/{output_file}") as writer:
         for root, dirs, files in os.walk(base_dir):
             if "supp" in dirs:
                 pmid = os.path.basename(root)
@@ -68,7 +70,6 @@ def read_table(base_dir: str = "./data", output_file: str = "output.xlsx"):
                 pdf_files = [f for f in os.listdir(supp_path) if f.lower().endswith(".pdf")]
                 for pdf in pdf_files:
                     pdf_path = os.path.join(supp_path, pdf)
-                    # print(pdf_path)
                     print(f"Processing: {pdf_path}")
-                    extract_table(pdf_path, writer, pmid)
+                    extract_table(pdf_path, pmid, root, writer)
     # print(f"Excel file saved to {output_file}")
