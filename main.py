934387a6aaa8a4e9943ce32c58ea0f286bfedcdf
Dickson <128027259+Yu-Chenxu@users.noreply.github.com>
2024-07-01 15:15:31 +0800

    add table_extractor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from crawler import Crawler
 import os
 from dotenv import load_dotenv
+from table_extractor import read_dir
 
 load_dotenv()
 PUBMED_API_KEY = os.environ['PUBMED_API_KEY']
@@ -9,3 +10,5 @@ crawler = Crawler()
 
 list = crawler.query(search_term="Meta analysis JAMA eye disease", max_results=10, api_key=PUBMED_API_KEY)
 crawler.download(list)
+
+read_dir() # read table from the pdf files in supplementary file and output the tables as excel format
