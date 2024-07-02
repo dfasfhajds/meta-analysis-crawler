20b2095a0ece6531568a0c14a7a83650737cc16d
Dickson <128027259+Yu-Chenxu@users.noreply.github.com>
2024-07-02 22:11:47 +0800

    Update ai_tool.py

--- ai_tool.py
+++ ai_tool.py
@@ -6,26 +6,29 @@ from dotenv import load_dotenv
 load_dotenv()
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 
-def get_quality_related_sections(html: str) -> list[int]:
+def get_quality_related_sections(text: str) -> str:
     try:
         client = OpenAI(api_key=OPENAI_API_KEY)
 
-        messages=[
+        messages = [
             {
                 'role': "system",
                 'content': """
+                You are particularly good at conducting meta-analysis on medicine articles.
                 Based on the provided caption of supplementary materials of a meta-analysis,
                 find sections that are related to quality assessment of the included studies.
-                Output the captions in a JSON object.
+                Output the captions in a JSON object. 
+                Only consider the parts that start with "eTable (this is a number e.g eTable 1.)."
+                "eTable." Should not be considered as a quality related section.
                 The JSON object must include "qa" field.
 
                 Example:
-                {"qa": ["eTable 4", "eTable 5"]}
+                {"qa": ["eTable 4. (name_of_the_table)", "eTable 5. (name_of_the_table)"]}
                 """
             },
             {
                 "role": "user",
-                "content": html,
+                "content": text,
             }
         ]
 
@@ -36,6 +39,9 @@ def get_quality_related_sections(html: str) -> list[int]:
         )
 
         return json.loads(chat_completion.choices[0].message.content)['qa']
+
     except Exception as e:
         print(f"Error when prompting gpt-3.5 to find quality related sections: {e}")
         return []
+    
+
