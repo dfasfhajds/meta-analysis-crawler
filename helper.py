bb9eb9af25af312326c28ee49e40d5d2b169c518
Iafcy622 <frankiecheung512@gmail.com>
2024-07-07 22:35:23 +0800

    Handled doi not found in xml response but appear in pubmed

--- helper.py
+++ helper.py
@@ -1,6 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 import random
+from dotenv import load_dotenv
+import os
+from xml.etree import ElementTree
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+load_dotenv()
+PUBMED_API_KEY = os.environ['PUBMED_API_KEY']
+
+session = requests.Session()
+retry = Retry(connect=3, backoff_factor=0.5)
+adapter = HTTPAdapter(max_retries=retry)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
 
 user_agents = [
     "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
@@ -42,7 +56,7 @@ def get_pdf_url_from_scihub(doi: str) -> str:
     Returns:
         str: URL of the article pdf
     """
-    full_text_response = requests.get(f"https://sci-hub.mksa.top/{doi}", headers=get_headers())
+    full_text_response = session.get(f"https://sci-hub.mksa.top/{doi}", headers=get_headers())
     soup = BeautifulSoup(full_text_response.content, "html.parser")
     
     try:
@@ -61,11 +75,15 @@ def get_pdf_url_from_pmc(pmc: str) -> str:
     Returns:
         str: URL of the article pdf
     """
-    full_text_response = requests.get(f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc}", headers=get_headers())
+    full_text_response = session.get(f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc}", headers=get_headers())
     soup = BeautifulSoup(full_text_response.content, "html.parser")
     try:
-        pdf_url = soup.find("li", {'class': "pdf-link"}).find("a").get("href")
-        return f"https://www.ncbi.nlm.nih.gov{pdf_url}"
+        pdf_link = soup.find("li", {'class': "pdf-link"})
+        if pdf_link:
+            pdf_url = pdf_link.find("a").get("href")
+            return f"https://www.ncbi.nlm.nih.gov{pdf_url}"
+        else:
+            return f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmc}/?report=printable"
     except Exception as e:
         return None
     
@@ -89,7 +107,23 @@ def get_pdf_url_from_doi_org(doi: str):
             if anchor.has_attr("data-article-url"):
                 return f"https://jamanetwork.com/{anchor.get('data-article-url')}"
     
-def get_pdf_url(pmc: str, doi: str):
+def get_doi_from_pmid(pmid: str):
+    url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+    params = {
+        'db': 'pubmed',
+        'id': pmid,
+        'retmode': 'xml',
+        'api_key': PUBMED_API_KEY
+    }
+    response = session.get(url, params=params, headers=get_headers())
+    xml_response = ElementTree.fromstring(response.content)
+    
+    try:
+        return xml_response.find(".//ArticleId[@IdType='doi']").text
+    except Exception as e:
+        return None
+
+def get_pdf_url(pmc: str, doi: str, pmid: str):
     """
     Get the URL of the article pdf given PMC and doi
 
@@ -105,6 +139,9 @@ def get_pdf_url(pmc: str, doi: str):
         if url:
             return url
     
+    if not doi and pmid:
+        doi = get_doi_from_pmid(pmid)
+
     if doi:
         url = get_pdf_url_from_scihub(doi)
         if url:
